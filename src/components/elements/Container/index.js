7a20a12462d064845de7b3f97c727b1f582d410c
David Martinez <davestringm@gmail.com>
2020-12-12 21:59:12 -0600

    add Container component

---
+++ src/components/elements/Container/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Box } from '@chakra-ui/react';
+
+function Container({ children, isFluid, ...rest }) {
+  return (
+    <Box
+      {...{
+        marginX: 'auto',
+        maxWidth: '1200px',
+        paddingX: 4,
+        position: 'relative',
+        width: '100%',
+        ...(isFluid && { maxWidth: '100%' }),
+        ...rest,
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
+
+export default Container;
