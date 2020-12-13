e965796f6af36f68cd8e67c70d2bc8eb74d29ad3
David Martinez <davestringm@gmail.com>
2020-12-12 22:03:11 -0600

    add the Header component and the HomePage container to the root route

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
+import { Box } from '@chakra-ui/react';
+
+import styled from '@emotion/styled';
+
+import Header from 'components/modules/Header';
+
+import HomePage from 'containers/HomePage/index.js';
+
+const Wrapper = styled(Box)`
+  background-color: ${({ theme }) => theme.colors.white};
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const ISOContainer = styled(Box)`
+  height: auto;
+  margin-top: ${({ theme }) => theme.space[16]};
+  position: relative;
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Header></Header>
+      <ISOContainer>
+        <HomePage></HomePage>
+      </ISOContainer>
+    </Wrapper>
+  );
 }
 
 export default App;
