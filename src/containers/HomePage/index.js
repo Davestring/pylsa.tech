a073015b71ac679761870d2362ee73806d1189e0
David Martinez <davestringm@gmail.com>
2020-12-12 22:02:04 -0600

    create HomePage container

---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Container from 'components/elements/Container';
+import H1 from 'components/elements/H1';
+
+function HomePage() {
+  return (
+    <Container>
+      <H1>Pylsa</H1>
+    </Container>
+  );
+}
+
+export default HomePage;
