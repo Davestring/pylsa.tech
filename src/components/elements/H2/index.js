c210059d0ab121e3ccc4f11c355b3056e7c655f4
David Martinez <davestringm@gmail.com>
2020-12-12 21:59:52 -0600

    add H2 component

---
+++ src/components/elements/H2/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Heading } from '@chakra-ui/react';
+
+function H2({ children, ...rest }) {
+  return (
+    <Heading {...{ as: 'h2', mb: 4, fontSize: 'xl', ...rest }}>
+      {children}
+    </Heading>
+  );
+}
+
+H2.propTypes = {
+  children: PropTypes.node,
+};
+
+export default H2;
