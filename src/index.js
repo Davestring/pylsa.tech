2316a183c1e355c4eb7e11d798795beddec7f5eb
David Martinez <davestringm@gmail.com>
2021-02-06 16:27:05 -0600

    add the router and redux configuration to the application index file

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import FontFaceObserver from 'fontfaceobserver';
 
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { ConnectedRouter } from 'connected-react-router';
+import { Provider } from 'react-redux';
 
 import App from 'containers/App';
 
+import configureAppStore from './store';
+import history from 'utils/history';
 import theme from 'styles/theme';
 
 import 'i18n';
 
 const robotoFontObserver = new FontFaceObserver('Roboto', {});
+const store = configureAppStore({}, history);
 const MOUNT_NODE = document.getElementById('root');
 
 // When Roboto is loaded, add a font-family using Roboto to the body
@@ -19,12 +24,16 @@ robotoFontObserver.load().then(() => {
 });
 
 const ThemeApp = ({ Component }) => (
-  <ChakraProvider theme={theme}>
-    <React.StrictMode>
-      <Component></Component>
-      <CSSReset></CSSReset>
-    </React.StrictMode>
-  </ChakraProvider>
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <ChakraProvider theme={theme}>
+        <React.StrictMode>
+          <Component></Component>
+          <CSSReset></CSSReset>
+        </React.StrictMode>
+      </ChakraProvider>
+    </ConnectedRouter>
+  </Provider>
 );
 
 const render = (Component) => {
