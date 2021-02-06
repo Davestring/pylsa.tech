9e4ea47cad2013fa9340971f8faff421a88c60b4
David Martinez <davestringm@gmail.com>
2021-02-06 17:07:08 -0600

    add the iconProps property to the SocialMedia component in order to pass props outside the component

--- src/components/modules/Footer/components/SocialMedia.js
+++ src/components/modules/Footer/components/SocialMedia.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 
 import { Icon, Link, Stack } from '@chakra-ui/react';
 
-function SocialMedia({ smItems, ...rest }) {
+function SocialMedia({ smItems, iconProps, ...rest }) {
   return (
     <Stack isInline {...rest}>
       {smItems.map((item) => (
         <Link key={`social-media-${item.name}`} href={item.link} isExternal>
-          <Icon as={item.icon} h={6} w={6}></Icon>
+          <Icon as={item.icon} {...iconProps}></Icon>
         </Link>
       ))}
     </Stack>
@@ -16,10 +16,15 @@ function SocialMedia({ smItems, ...rest }) {
 }
 
 SocialMedia.defaultProps = {
+  iconProps: { height: 6, width: 6 },
   smItems: [],
 };
 
 SocialMedia.propTypes = {
+  iconProps: PropTypes.shape({
+    height: PropTypes.number,
+    width: PropTypes.number,
+  }),
   smItems: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string,
