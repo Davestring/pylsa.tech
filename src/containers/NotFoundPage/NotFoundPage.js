dd67517fe3a97aa076eb0f9c798247c2f5d0e4d0
David Martinez <davestringm@gmail.com>
2021-02-06 21:18:42 -0600

    refactor the NotFoundPage container

---
+++ src/containers/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Heading } from '@chakra-ui/react';
+
+import Container from 'components/elements/Container';
+
+function NotFoundPage(props) {
+  return (
+    <Container>
+      <Heading as="h1">Not Found Page</Heading>
+    </Container>
+  );
+}
+
+export default NotFoundPage;
