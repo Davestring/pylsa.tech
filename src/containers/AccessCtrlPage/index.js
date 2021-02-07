5082030ed12d4fbe7336b9d7a56f8ee5698d0641
David Martinez <davestringm@gmail.com>
2021-02-06 18:28:29 -0600

    add the Access Control product page

---
+++ src/containers/AccessCtrlPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Heading } from '@chakra-ui/react';
+
+import Container from 'components/elements/Container';
+
+function AccessCtrlPage() {
+  return (
+    <Container>
+      <Heading as="h1">Access Control Page</Heading>
+    </Container>
+  );
+}
+
+export default AccessCtrlPage;
