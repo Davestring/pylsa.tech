cf4ce3c119e7ee95a3a60899962af9c0a9db9b00
David Martinez <davestringm@gmail.com>
2021-02-06 19:59:16 -0600

    add content to the AccessControlPage

--- src/containers/AccessCtrlPage/index.js
+++ src/containers/AccessCtrlPage/index.js
@@ -1,15 +1,1 @@
-import React from 'react';
-
-import { Heading } from '@chakra-ui/react';
-
-import Container from 'components/elements/Container';
-
-function AccessCtrlPage() {
-  return (
-    <Container>
-      <Heading as="h1">Access Control Page</Heading>
-    </Container>
-  );
-}
-
-export default AccessCtrlPage;
+export { default } from './AccessCtrlPage';
