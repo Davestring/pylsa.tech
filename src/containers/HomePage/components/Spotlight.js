23acae2f5632dad5e4eb71849a515e7b42cad927
David Martinez <davestringm@gmail.com>
2021-02-07 00:37:30 -0600

    solve the 100vh problem when opening the application on a mobile browser

--- src/containers/HomePage/components/Spotlight.js
+++ src/containers/HomePage/components/Spotlight.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Stack, Text } from '@chakra-ui/react';
 
+import { use100vh } from 'react-div-100vh';
+
 import { useTranslation } from 'react-i18next';
 
 import Container from 'components/elements/Container';
@@ -9,16 +11,11 @@ import H1 from 'components/elements/H1';
 
 function Spotlight({ ...rest }) {
   const { t } = useTranslation('homepage');
+  const height = use100vh();
 
   return (
-    <Container
-      bg="base"
-      h="calc(100vh - 4rem)"
-      textAlign="center"
-      isFluid
-      {...rest}
-    >
-      <Stack align="center" justify="center" h="100%">
+    <Container bg="base" textAlign="center" isFluid {...rest}>
+      <Stack align="center" justify="center" h={`calc(${height}px - 4rem)`}>
         <H1 color="white" fontSize="6xl" textTransform="uppercase">
           {t('title')}
         </H1>
