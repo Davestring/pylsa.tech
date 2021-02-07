9e2ef735b24838f62689b7f171c2180802f66eaf
David Martinez <davestringm@gmail.com>
2021-02-06 20:05:29 -0600

    add the CCTVPage container and update the content

--- src/containers/CCTVPage/index.js
+++ src/containers/CCTVPage/index.js
@@ -1,15 +1,1 @@
-import React from 'react';
-
-import { Heading } from '@chakra-ui/react';
-
-import Container from 'components/elements/Container';
-
-function CCTVPage() {
-  return (
-    <Container>
-      <Heading as="h1">CCTV Page</Heading>
-    </Container>
-  );
-}
-
-export default CCTVPage;
+export { default } from './CCTVPage';
