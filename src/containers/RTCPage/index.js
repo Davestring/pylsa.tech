6bc2b39f2b9e8a301469c7e620aaab121f17a100
David Martinez <davestringm@gmail.com>
2021-02-06 18:29:59 -0600

    add the RTC product page

---
+++ src/containers/RTCPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Heading } from '@chakra-ui/react';
+
+import Container from 'components/elements/Container';
+
+function RTCPage() {
+  return (
+    <Container>
+      <Heading as="h1">RTC Page</Heading>
+    </Container>
+  );
+}
+
+export default RTCPage;
