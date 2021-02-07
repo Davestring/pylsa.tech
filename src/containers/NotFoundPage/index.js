147ed0c59bc9eae5b7902693215b249712b7bff9
David Martinez <davestringm@gmail.com>
2021-02-06 18:30:19 -0600

    update the Not Found page

--- src/containers/NotFoundPage/index.js
+++ src/containers/NotFoundPage/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-import { Flex, Heading } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
 
 import Container from 'components/elements/Container';
 
 function NotFoundPage(props) {
   return (
-    <Container h="calc(100vh - 4rem - 203px)">
-      <Flex align="center" h="100%">
-        <Heading as="h1">Not Found Page</Heading>
-      </Flex>
+    <Container>
+      <Heading as="h1">Not Found Page</Heading>
     </Container>
   );
 }
