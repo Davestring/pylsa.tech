1ad6913d7471a224811ee3e6b71b13a4ff617d46
David Martinez <davestringm@gmail.com>
2021-02-06 21:30:25 -0600

    add Helmet in every page and improve the SEO

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
 import { Box } from '@chakra-ui/react';
+import { Helmet } from 'react-helmet';
+
+import { useTranslation } from 'react-i18next';
 
 import Products from 'containers/HomePage/components/Products';
 import Spotlight from 'containers/HomePage/components/Spotlight';
 
 function HomePage() {
+  const { t } = useTranslation('homepage');
   return (
-    <Box as="article">
-      <Spotlight as="section"></Spotlight>
-      <Products as="section"></Products>
-    </Box>
+    <>
+      <Helmet>
+        <title>{t('html.title')}</title>
+        <meta name="description" content={t('html.meta')} />
+      </Helmet>
+      <Box as="article">
+        <Spotlight as="section"></Spotlight>
+        <Products as="section"></Products>
+      </Box>
+    </>
   );
 }
 
