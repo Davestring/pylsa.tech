20489480a9a46ce151c67587f2c921fba6d6629d
David Martinez <davestringm@gmail.com>
2021-02-06 21:09:47 -0600

    add content to the FirePrevPage container

--- src/containers/FirePrevPage/index.js
+++ src/containers/FirePrevPage/index.js
@@ -1,15 +1,1 @@
-import React from 'react';
-
-import { Heading } from '@chakra-ui/react';
-
-import Container from 'components/elements/Container';
-
-function FirePrevPage() {
-  return (
-    <Container>
-      <Heading as="h1">Fire Prev Page</Heading>
-    </Container>
-  );
-}
-
-export default FirePrevPage;
+export { default } from './FirePrevPage';
