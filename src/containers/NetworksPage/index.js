d8f859b069243904a01a9aaadaa319aacd0d31fa
David Martinez <davestringm@gmail.com>
2021-02-06 21:10:24 -0600

    add content to the NetworksPage container

--- src/containers/NetworksPage/index.js
+++ src/containers/NetworksPage/index.js
@@ -1,15 +1,1 @@
-import React from 'react';
-
-import { Heading } from '@chakra-ui/react';
-
-import Container from 'components/elements/Container';
-
-function NetworksPage() {
-  return (
-    <Container>
-      <Heading as="h1">Networks Page</Heading>
-    </Container>
-  );
-}
-
-export default NetworksPage;
+export { default } from './NetworksPage';
