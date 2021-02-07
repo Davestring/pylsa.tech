9467fa787f0115e02bd2b8ca5c8403a5abd25cf1
David Martinez <davestringm@gmail.com>
2021-02-06 21:00:18 -0600

    add the locales files into the i18n file

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -8,8 +8,12 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 import accessControl_es from 'i18n/locales/es/access-control-page.json';
 import cctv_es from 'i18n/locales/es/cctv-page.json';
+import fireprev_es from 'i18n/locales/es/fire-prev-page.json';
 import footer_es from 'i18n/locales/es/footer.json';
 import homepage_es from 'i18n/locales/es/home-page.json';
+import ips_es from 'i18n/locales/es/ips-page.json';
+import networks_es from 'i18n/locales/es/networks-page.json';
+import rtc_es from 'i18n/locales/es/rtc-page.json';
 
 const translationsJson = {
   es: {
@@ -17,6 +21,10 @@ const translationsJson = {
     cctv: cctv_es,
     footer: footer_es,
     homepage: homepage_es,
+    fireprev: fireprev_es,
+    ips: ips_es,
+    networks: networks_es,
+    rtc: rtc_es,
   },
 };
 
@@ -28,7 +36,7 @@ i18n
     debug: process.env.NODE_ENV === 'development',
     fallbackLng: 'es',
     lng: 'es',
-    ns: ['accessControl', 'footer', 'homepage'],
+    ns: ['homepage'],
     interpolation: {
       escapeValue: true,
     },
