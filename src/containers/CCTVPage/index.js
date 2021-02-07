ea9b4fe647162a3c13aec7df47638ff5f2dabe1f
David Martinez <davestringm@gmail.com>
2021-02-06 18:28:00 -0600

    add the CCTV product page

---
+++ src/containers/CCTVPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Heading } from '@chakra-ui/react';
+
+import Container from 'components/elements/Container';
+
+function CCTVPage() {
+  return (
+    <Container>
+      <Heading as="h1">CCTV Page</Heading>
+    </Container>
+  );
+}
+
+export default CCTVPage;
