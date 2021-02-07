ee1b2e38cb78d95010aecca75a00d0f64deaa10d
David Martinez <davestringm@gmail.com>
2021-02-06 21:10:07 -0600

    add content to the IPSPage container

--- src/containers/IPSPage/index.js
+++ src/containers/IPSPage/index.js
@@ -1,15 +1,1 @@
-import React from 'react';
-
-import { Heading } from '@chakra-ui/react';
-
-import Container from 'components/elements/Container';
-
-function IPSPage() {
-  return (
-    <Container>
-      <Heading as="h1">IPS Page</Heading>
-    </Container>
-  );
-}
-
-export default IPSPage;
+export { default } from './IPSPage';
