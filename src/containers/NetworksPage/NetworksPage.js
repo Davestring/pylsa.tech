05095d70fbc52313188f8afb937acbaa40d47b25
David Martinez <davestringm@gmail.com>
2021-02-20 20:53:41 -0600

    implement the LogoAnimatedGrid in the network product page

--- src/containers/NetworksPage/NetworksPage.js
+++ src/containers/NetworksPage/NetworksPage.js
@@ -7,6 +7,9 @@ import { Box, Heading, Text } from '@chakra-ui/react';
 import { Trans, useTranslation } from 'react-i18next';
 
 import Container from 'components/elements/Container';
+import LogoAnimatedGrid from 'components/elements/LogoAnimatedGrid';
+
+import LOGOS from './logos';
 
 function NetworksPage() {
   const { t } = useTranslation('networks');
@@ -25,14 +28,19 @@ function NetworksPage() {
           textAlign="center"
           isFluid
         >
-          <Heading as="h1" color="white" textTransform="uppercase">
+          <Heading as="h1" color="white" textTransform="uppercase" px={4}>
             {t('title')}
           </Heading>
         </Container>
         <Container as="section">
           <Trans i18nKey="networks:description">
-            <Text fontSize="lg" textAlign="justify"></Text>
+            <Text fontSize="lg" textAlign="justify" mb={12}></Text>
           </Trans>
+          <LogoAnimatedGrid
+            columns={{ base: 2, md: 3 }}
+            logos={LOGOS}
+            spacing={12}
+          ></LogoAnimatedGrid>
         </Container>
       </Box>
     </>
