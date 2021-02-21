35f5e4edd18d9e04f7d5576c5e9ad4e6be8aeed2
David Martinez <davestringm@gmail.com>
2021-02-21 12:43:58 -0600

    add mailto link in OtherContact email section

--- src/containers/ContactPage/components/OtherContact.js
+++ src/containers/ContactPage/components/OtherContact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Box, Divider, Icon, Stack, Text } from '@chakra-ui/react';
+import { Box, Divider, Icon, Link, Stack, Text } from '@chakra-ui/react';
 
 import { FaPhoneSquare } from 'react-icons/fa';
 import { GrMail } from 'react-icons/gr';
@@ -21,7 +21,11 @@ function OtherContact({ ...rest }) {
       <Box bg="gray.50" fontSize="sm" p={4}>
         <Stack isInline align="center" spacing={4}>
           <Icon as={GrMail} h={6} w={6}></Icon>
-          <Text>{t('other.email')}</Text>
+          <Link href="mailto:contacto@pylsa.tech" isExternal>
+            <Text as="strong" color="blue.500">
+              {t('other.email')}
+            </Text>
+          </Link>
         </Stack>
         <Divider my={4}></Divider>
         <Stack isInline align="center" spacing={4}>
