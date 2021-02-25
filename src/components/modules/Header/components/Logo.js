4bc430faeac9ad6147f4e4c87fa1177295d1f903
David Martinez <davestringm@gmail.com>
2021-02-25 17:24:00 -0600

    add background color transition to navbar when user scrolls

--- src/components/modules/Header/components/Logo.js
+++ src/components/modules/Header/components/Logo.js
@@ -6,10 +6,14 @@ import { Link as ReachLink } from 'react-router-dom';
 import { Image, Link, useBreakpointValue } from '@chakra-ui/react';
 
 import PylsaLogo from 'images/pylsa-logo.png';
+import PylsaLogoBg from 'images/pylsa-logo-bg.png';
 import PylsaLogoSmall from 'images/pylsa-logo-small.png';
 
-function Logo({ setSelected, width }) {
-  const logo = useBreakpointValue({ base: PylsaLogoSmall, lg: PylsaLogo });
+function Logo({ isTransparent, setSelected, width }) {
+  const logo = useBreakpointValue({
+    base: PylsaLogoSmall,
+    lg: isTransparent ? PylsaLogoBg : PylsaLogo,
+  });
   return (
     <Link
       as={ReachLink}
@@ -23,11 +27,13 @@ function Logo({ setSelected, width }) {
 }
 
 Logo.defaultProps = {
+  isTransparent: false,
   setSelected: () => {},
   width: { base: 55, lg: 120 },
 };
 
 Logo.propTypes = {
+  isTransparent: PropTypes.bool,
   setSelected: PropTypes.func,
   width: PropTypes.oneOfType([
     PropTypes.number,
